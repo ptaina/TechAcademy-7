7873ae927423537afc26cb7fc59ef2bab4b899c4
ptaina <tainapereirae27@gmail.com>
2025-09-12 04:00:52 -0300

    feat(auth): protects routes with JWT middleware
    Implements the authentication and authorization layer. Creates a reusable authMiddleware, adds authorization logic to controllers, and applies it to secure routes.

--- backend/src/routes/producerRoutes.ts
+++ backend/src/routes/producerRoutes.ts
@@ -7,12 +7,14 @@ import {
   destroyProducerById,
 } from "../controllers/producerController";
 
+import { authMiddleware } from "../middleware/authMiddleware";
+
 const router = express.Router();
 
 router.post("/producers", createProducer);
-router.get("/producers/:id", getProducerById);
-router.put("/producers/:id", updateProducer);
-router.put("/producers/:id/password", updateProducerPassword);
-router.delete("/producers/:id", destroyProducerById);
+router.get("/producers/:id", authMiddleware, getProducerById);
+router.put("/producers/:id", authMiddleware, updateProducer);
+router.put("/producers/:id/password", authMiddleware, updateProducerPassword);
+router.delete("/producers/:id", authMiddleware, destroyProducerById);
 
 export default router;
